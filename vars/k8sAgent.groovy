2a26fecf448049457bda3d73b2a889ec2642b8bf
Balavishnu <71483701+Vishnu857@users.noreply.github.com>
2026-06-25 23:01:08 +0530

    Creating a hello world cicd pipeline using python

--- vars/k8sAgent.groovy
+++
@@ -1,8 +0,0 @@
-def call(Map config = [:]) {
-    return [
-        cloud: config.cloud ?: 'deploy',
-        inheritFrom: config.podTemplate,
-        defaultContainer: config.containerName ?: 'jnlp',
-        serviceAccount: config.serviceAccount ?: 'jenkins'
-    ]
-}
