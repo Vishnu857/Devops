fd1f07caa36c5e8e02b18ff731b123bd6b7fd184
vishnu857 <vishnuchandran2532@gmail.com>
2026-08-09 09:16:03 +0530

    updated workflow

--- scripts/main.py
+++ scripts/main.py
@@ -2,9 +2,12 @@ print("Hello world")
 import os
 
 Choice = os.getenv("Choice")
-
-if (Choice=="Run"):
+parameter = os.getenv("parameter")
+if (Choice=="Run" & parameter=="start"):
   print("running")
+elif (Choice == "stop" & parameter == "start" ):
+  print("stopping with error")
+  raise Exception("Something went wrong")
 else:
-  print("stopped")
-print(f"Build triggered by: {Choice}")
+  print ("stopped officially")
+print(f"Build triggered by: /n Choice:{Choice} parameter:{parameter}")
